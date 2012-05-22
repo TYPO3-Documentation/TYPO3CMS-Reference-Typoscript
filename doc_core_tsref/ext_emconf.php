345de878617cc0fc965a4d1ca456f7ae9a2ce838
Christopher <chrissitopher@gmx.de>
2012-05-22 18:39:04 +0200

    [TASK] Migrate doc_core_tsref 4.5
    Import doc_core_tsref for TYPO3 4.5.
    
    Change-Id: I1bf22c8aa3990403f64fce6220f3fc0e7f2b1723

--- doc_core_tsref/ext_emconf.php
+++ doc_core_tsref/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_tsref".
 #
-# Auto generated 10-04-2011 21:14
+# Auto generated 06-09-2011 00:52
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,17 +15,18 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'TypoScript Reference - TSref - is a true reference describing the core Content Objects and functions available for Template building using the TypoScript template engine.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.4.0',
+	'version' => '4.5.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
+	'doNotLoadInFE' => 1,
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author' => 'Official Documentation',
 	'author_email' => 'documentation@typo3.org',
@@ -40,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"5065";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"f9d3";s:22:"doc/doc_core_tsref.txt";s:4:"8cbe";s:14:"doc/manual.sxw";s:4:"633e";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"b14d";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"c553";s:22:"doc/doc_core_tsref.txt";s:4:"07d3";s:14:"doc/manual.sxw";s:4:"b9da";}',
 	'suggests' => array(
 	),
 );
