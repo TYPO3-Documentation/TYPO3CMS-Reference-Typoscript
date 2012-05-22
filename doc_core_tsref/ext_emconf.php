be20b71daf2908b84d9e5d2706817ca1cca16e61
Christopher <chrissitopher@gmx.de>
2012-05-22 18:36:38 +0200

    [TASK] Migrate doc_core_tsref 4.3
    Import doc_core_tsref for TYPO3 4.3.
    
    Change-Id: I4450b5c6577ac6515109566be105d5f1b1a671e3

--- doc_core_tsref/ext_emconf.php
+++ doc_core_tsref/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "doc_core_tsref"
 #
-# Auto generated 13-05-2009 18:12
+# Auto generated 12-02-2010 10:06
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'TypoScript Reference - TSref - is a true reference describing the core Content Objects and functions available for Template building using the TypoScript template engine.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.2.1',
+	'version' => '4.3.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"d450";s:8:"TODO.txt";s:4:"2bf0";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"f7ff";s:14:"doc/manual.sxw";s:4:"4ffb";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"4866";s:8:"TODO.txt";s:4:"2bf0";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"dc26";s:22:"doc/doc_core_tsref.txt";s:4:"0105";s:14:"doc/manual.sxw";s:4:"d05a";}',
 	'suggests' => array(
 	),
 );
