e15a3aa07be6dc5628096003be0de071efad2413
Christopher <chrissitopher@gmx.de>
2012-05-22 17:36:30 +0200

    [TASK] Migrate doc_core_tsconfig 4.4
    Import doc_core_tsconfig for TYPO3 4.4.
    
    Change-Id: Idb9ecad59e5985ac2b137984d6a93e71cb28f5e3

--- Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
+++ Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "doc_core_tsconfig"
+# Extension Manager/Repository config file for ext "doc_core_tsconfig".
 #
-# Auto generated 12-02-2010 17:20
+# Auto generated 30-12-2010 21:50
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Page TSconfig and User TSconfig reference.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.3.2',
+	'version' => '4.4.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -27,8 +27,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'TYPO3 Core Development Team',
-	'author_email' => 'info@typo3.org',
+	'author' => 'Documentation Team',
+	'author_email' => 'documentation@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"bc58";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"b573";s:25:"doc/doc_core_tsconfig.txt";s:4:"f6cc";s:14:"doc/manual.sxw";s:4:"3e8a";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9598";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"e7ad";s:25:"doc/doc_core_tsconfig.txt";s:4:"2838";s:14:"doc/manual.sxw";s:4:"d629";}',
 	'suggests' => array(
 	),
 );
