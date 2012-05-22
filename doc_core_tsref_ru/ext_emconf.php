25406b926bdb0901dcbfdf4a7d1cfa1618d7427e
Christopher <chrissitopher@gmx.de>
2012-05-22 18:47:01 +0200

    [TASK] Migrate doc_core_tsref_ru 4.5
    Import doc_core_tsref_ru for TYPO3 4.5.
    
    Change-Id: I3acb0d8641ba186a3fb43caed11a0c454842ddca

---
+++ doc_core_tsref_ru/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_core_tsref".
+#
+# Auto generated 16-05-2011 22:05
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Russian translation of TSref',
+	'description' => 'TypoScript Reference - translated into Russian. TSref - is a true reference describing the core Content Objects and functions available for Template building using the TypoScript template engine.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '4.5.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"6dfd";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"ccd3";s:22:"doc/doc_core_tsref.txt";s:4:"8c01";s:14:"doc/manual.sxw";s:4:"b666";}',
+	'suggests' => array(
+	),
+);
+
+?>
