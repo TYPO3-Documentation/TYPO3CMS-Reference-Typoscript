db2bae3cfa8209920fd913e8afd9447e26fbb180
Christopher <chrissitopher@gmx.de>
2012-05-22 18:35:44 +0200

    [TASK] Migrate doc_core_tsref 4.2
    Import doc_core_tsref for TYPO3 4.2.
    
    Change-Id: I7f8b90d700f2d127f14de73658a3f9f93a33a94f

---
+++ doc_core_tsref/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "doc_core_tsref"
+#
+# Auto generated 13-05-2009 18:12
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TSref',
+	'description' => 'TypoScript Reference - TSref - is a true reference describing the core Content Objects and functions available for Template building using the TypoScript template engine.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '4.2.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'TYPO3 Core Development Team',
+	'author_email' => 'info@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"d450";s:8:"TODO.txt";s:4:"2bf0";s:12:"ext_icon.gif";s:4:"7277";s:22:"doc/doc_core_tsref.pdf";s:4:"f7ff";s:14:"doc/manual.sxw";s:4:"4ffb";}',
+	'suggests' => array(
+	),
+);
+
+?>
