851ac4235f4760c35b0fbe7b0138cf5533c22fea
Christopher <chrissitopher@gmx.de>
2012-05-22 17:37:39 +0200

    [TASK] styleguide: Migrate doc_core_tsconfig 4.5
    Import doc_core_tsconfig for TYPO3 4.5.
    
    Change-Id: I21805da2ef7deb5f2b871af0ff0c8fe4071966f0

--- Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
+++ Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_tsconfig".
 #
-# Auto generated 30-12-2010 21:50
+# Auto generated 06-09-2011 00:30
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,19 +15,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Page TSconfig and User TSconfig reference.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.4.0',
+	'version' => '4.5.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
+	'doNotLoadInFE' => 1,
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Documentation Team',
+	'author' => 'Official Documentation',
 	'author_email' => 'documentation@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
@@ -40,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9598";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"e7ad";s:25:"doc/doc_core_tsconfig.txt";s:4:"2838";s:14:"doc/manual.sxw";s:4:"d629";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"22ee";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"6350";s:25:"doc/doc_core_tsconfig.txt";s:4:"7410";s:14:"doc/manual.sxw";s:4:"6a8a";}',
 	'suggests' => array(
 	),
 );
