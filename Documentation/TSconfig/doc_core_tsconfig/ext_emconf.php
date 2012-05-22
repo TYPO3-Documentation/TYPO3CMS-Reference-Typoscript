e8bffff50bba353843a63547948c03061e829159
Christopher <chrissitopher@gmx.de>
2012-05-22 17:38:21 +0200

    [TASK] Migrate doc_core_tsconfig 4.6
    Import doc_core_tsconfig for TYPO3 4.6.
    
    Change-Id: Icbfff203ef155dce07437d7007fd00bcbccddbcf

--- Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
+++ Documentation/TSconfig/doc_core_tsconfig/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_tsconfig".
 #
-# Auto generated 06-09-2011 00:30
+# Auto generated 27-10-2011 21:04
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Page TSconfig and User TSconfig reference.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.5.2',
+	'version' => '4.6.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"22ee";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"6350";s:25:"doc/doc_core_tsconfig.txt";s:4:"7410";s:14:"doc/manual.sxw";s:4:"6a8a";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"19e3";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_tsconfig.pdf";s:4:"0763";s:25:"doc/doc_core_tsconfig.txt";s:4:"5a76";s:14:"doc/manual.sxw";s:4:"b675";}',
 	'suggests' => array(
 	),
 );
