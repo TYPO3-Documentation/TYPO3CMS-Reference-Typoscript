2539a287b64012a38427c91c8943199a539caff4
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-06-23 14:55:18 +0200

    [TASK] styleguide: Move code-snippet creation to new tool (#277)
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>

---
+++ Documentation/TSconfig/Documentation/CodeSnippets/codesnippets.php
@@ -0,0 +1,20 @@
+<?php
+// https=>//github.com/TYPO3-Documentation/t3docs-codesnippets
+// ddev exec vendor/bin/typo3  restructured_api_tools:php_domain public/fileadmin/TYPO3CMS-Reference-TSconfig/Documentation/CodeSnippets/
+
+return [
+    [
+        'action' => 'createCodeSnippet',
+        'sourceFile' => 'typo3conf/ext/examples/Configuration/TSconfig/Page/Mod/csvExport.tsconfig',
+        'targetFileName' => 'PageTSconfig/Mod/CsvExport.rst.txt',
+        'caption' => 'EXT:examples/Configuration/TSconfig/Page/Mod/csvExport.tsconfig',
+        'language' => 'typoscript',
+    ],
+    [
+        'action' => 'createCodeSnippet',
+        'sourceFile' => 'typo3conf/ext/examples/Configuration/TSconfig/Page/Mod/noExportRecordsLinks.tsconfig',
+        'targetFileName' => 'PageTSconfig/Mod/noExportRecordsLinks.rst.txt',
+        'caption' => 'EXT:examples/Configuration/TSconfig/Page/Mod/noExportRecordsLinks.tsconfig',
+        'language' => 'typoscript',
+    ]
+];
