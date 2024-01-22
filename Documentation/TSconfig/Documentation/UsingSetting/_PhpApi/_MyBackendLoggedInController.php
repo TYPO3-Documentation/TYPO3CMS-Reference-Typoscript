f6c893fa9f365925e5d76ea52967bf24cd2018ef
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-22 13:15:41 +0000

    [CI] Use php-cs-fixer directly (#420)
    The typo3/coding-standards package isn't compatible with Symfony 7 yet. But as this is only
    a proxy package for php-cs-fixer defining some rules, we can use php-cs-fixer directly.
    
    The configuration from the TYPO3 Core is used.
    
    See: https://github.com/TYPO3/typo3/blob/main/Build/php-cs-fixer/config.php
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/786
    Releases: main, 12.4

--- Documentation/TSconfig/Documentation/UsingSetting/_PhpApi/_MyBackendLoggedInController.php
+++ Documentation/TSconfig/Documentation/UsingSetting/_PhpApi/_MyBackendLoggedInController.php
@@ -18,6 +18,6 @@ final class _MyBackendLoggedInController
 
     private function getUserTsConfig(): array
     {
-        return  $GLOBALS['BE_USER']->getTSConfig();
+        return $GLOBALS['BE_USER']->getTSConfig();
     }
 }
