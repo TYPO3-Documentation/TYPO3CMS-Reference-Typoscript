5741d613797c6aec67553c58202c368812473c30
lina.wolf <lwolf@w-commerce.de>
2024-05-09 19:57:52 +0200

    [FEATURE] Add TypoScript provider for sites and sets
    Releases: main

---
+++ Documentation/UsingSetting/_Sets/_TcaOverridesSystemplateV12.php
@@ -0,0 +1,25 @@
+<?php
+
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+defined('TYPO3') or die();
+
+call_user_func(function()
+{
+    $extensionKey = 'my-extension';
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    if ($versionInformation->getMajorVersion() < 13) {
+        ExtensionManagementUtility::addStaticFile(
+            $extensionKey,
+            'Configuration/Sets/MyExtension',
+            'My Extension, main TypoScript, always include'
+        );
+        ExtensionManagementUtility::addStaticFile(
+            $extensionKey,
+            'Configuration/Sets/MyExtensionWithACoolFeature',
+            'My Extension, Cool feature'
+        );
+    }
+});
---
+++ Documentation/UsingSetting/_Sets/_ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+ExtensionManagementUtility::addTypoScript(
+    'my_extension',
+    'setup',
+    "@import 'EXT:my_extension/Configuration/TypoScript/setup.typoscript'"
+);
